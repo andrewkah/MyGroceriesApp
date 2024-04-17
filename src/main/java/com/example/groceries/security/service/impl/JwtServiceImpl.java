d13bd48340df74a813a7271c0db71b872204b60d
andrewkah <107668588+andrewkah@users.noreply.github.com>
2024-04-17 16:53:41 +0300

    Update JwtServiceImpl.java

--- src/main/java/com/example/groceries/security/service/impl/JwtServiceImpl.java
+++ src/main/java/com/example/groceries/security/service/impl/JwtServiceImpl.java
@@ -27,7 +27,7 @@ public class JwtServiceImpl implements JwtService {
                 .builder()
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) /// Expiration after one hour
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) /// Expiration after Ten hour
                 .signWith(setSigninKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
